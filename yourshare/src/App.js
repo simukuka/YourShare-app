31dc1b3b0e2ec25bb524f57ece9e7ad5c87d646d
Simukuka <simukukaaka@gmail.com>
2024-07-18 14:25:26 -0700

    added routes

--- yourshare/src/App.js
+++ yourshare/src/App.js
@@ -3,36 +3,21 @@ import "./App.css";
 import { WelcomePage } from "./pages/WelcomePage";
 import { AddItemPage } from "./pages/AddItemPage";
 import { CommunityPage } from "./pages/CommunityPage";
+import { Routes, Route, Link } from 'react-router-dom';
 
-export const pages = {
-  WelcomePage: "WelcomePage",
-  CommunityPage: "CommunityPage",
-  AddItemPage: "AddItemPage",
-};
 
 function App() {
-  const [currentPage, setCurrentPage] = React.useState(pages.AccountPage);
 
-  const componentToShow = React.useMemo(() => {
-    let whichComponentToShow;
-
-    console.log("currentPage: " + currentPage);
-    switch (currentPage) {
-      case pages.CommunityPage:
-        whichComponentToShow = <CommunityPage changePage={setCurrentPage} />;
-        break;
-      case pages.AddItemPage:
-        whichComponentToShow = <AddItemPage changePage={setCurrentPage} />;
-        break;
-      case pages.WelcomePage:
-      default:
-        whichComponentToShow = <WelcomePage changePage={setCurrentPage} />;
-        break;
-    }
-    return whichComponentToShow;
-  }, [currentPage, setCurrentPage]);
-
-  return <div className="App">{componentToShow}</div>;
+  return(
+  
+  <div className="App">
+    <Routes>
+      <Route path="/" element={<WelcomePage/>}/>
+      <Route path="/community" element={<CommunityPage/>}/>
+      <Route path="/additem" element={<AddItemPage/>}/>
+      <Route path="*" element={<div>Page Not Found 484</div>}/>
+    </Routes>
+</div>);
 }
 
 export default App;
--- yourshare/src/pages/CommunityPage.js
+++ yourshare/src/pages/CommunityPage.js
@@ -1,10 +1,29 @@
 import React from "react";
 // import { pages } from "../App";
+import { Link, useNavigate } from "react-router-dom";
+
+
+export const WelcomePage = (props) => {
+
+  };
 
 export const CommunityPage = (props) => {
+    const navigate = useNavigate();
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    navigate("/additem")
+  }
   return (
     <div>
       <h1>Community</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name"></input>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <p> <Link to="/additem">Sign In</Link></p>
     </div>
   );
 };
