02de5200c184456ed91a97aa2698cac01459e5b0
Lee Caceres <amecac21@gmail.com>
2024-07-23 16:44:42 -0700

    fixing merge conflicts

--- yourshare/src/index.js
+++ yourshare/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
