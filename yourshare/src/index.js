d8d811acc689f098fa6b4f830e75d3d91b06edda
Lee Caceres <amecac21@gmail.com>
2024-07-23 17:08:55 -0700

    test

--- yourshare/src/index.js
+++ yourshare/src/index.js
@@ -17,7 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <WelcomePage />
     </Router>
 
   </React.StrictMode>
