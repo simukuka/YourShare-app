28f0458de035da9f2c404d613bf44d3b0c4dc91e
Simukuka <simukukaaka@gmail.com>
2024-07-25 08:43:36 -0700

    modified community page

--- yourshare/src/pages/CommunityPage.js
+++ yourshare/src/pages/CommunityPage.js
@@ -12,9 +12,9 @@ import Y_s from "../images/back-button.png";
 export const CommunityPage = (props) => {
   const navigate = useNavigate();
   const [friends, setFriends] = useState([
-    { name: 'Stacey', borrowed: 'None', lent: 'None', bestFriend: true },
+    { name: 'Stacey', borrowed: 'None', lent: 'Back Pack', bestFriend: true },
     { name: 'Marcos', borrowed: 'None', lent: 'None', bestFriend: false },
-    { name: 'Cam', borrowed: 'None', lent: 'None', bestFriend: true },
+    { name: 'Cam', borrowed: 'Note Book', lent: 'None', bestFriend: true },
     { name: 'Jim', borrowed: 'None', lent: 'None', bestFriend: false }
   ]);
   const [newFriend, setNewFriend] = useState({ name: '', borrowed: 'None', lent: 'None', bestFriend: false });
@@ -68,10 +68,10 @@ export const CommunityPage = (props) => {
             </Table>
           </Col>
         </Row>
-        <Row>
+        <Row  style={{ textAlign: 'left' }} >
           <Col md="12">
             <Form onSubmit={handleAddFriend}>
-              <Row>
+              <Row >
                 <Col md="3">
                   <Form.Group controlId="formFriendName">
                     <Form.Control
@@ -80,19 +80,19 @@ export const CommunityPage = (props) => {
                       name="name"
                       value={newFriend.name}
                       onChange={handleNewFriendChange}
-                      required
+
                     />
                   </Form.Group>
                 </Col>
               </Row>
-              <Col md="2" className="d-flex align-items-end" style={{ paddingTop: 12 }}>
+              <Col md="4" className="d-flex align-items-end" style={{ paddingTop: 12 }}>
                 <Button variant="secondary" type="submit">Add Friend</Button>
               </Col>
             </Form>
           </Col>
         </Row>
-        <Row className="mt-3">
-          <Col md="12">
+        <Row className="mt-3" style={{ textAlign: 'left' }}>
+          <Col md="12" >
             <Form style={{ textAlign: 'left' }}>
               <Form.Check type="checkbox" label="Text me when someone wants to borrow an item" defaultChecked />
               <Form.Check type="checkbox" label="Allow best friends to auto borrow without approval" defaultChecked />
